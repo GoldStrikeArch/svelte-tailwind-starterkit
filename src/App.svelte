f2e4c5e883ee78276595ff9ba94ffe493c080493
Mihail Pertsev <arthasmontano@mail.ru>
2020-10-03 19:09:14 +0300

    working

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,9 @@
 <script>
-  let count = 0;
-  function handleClick() {
-    count++;
-  }
-
-  let disabled = true;
+  import Landing from "./components/Landing/Landing.svelte";
+  import Navbar from "./components/Navbar/Navbar.svelte";
+  import Footer from "./components/Footer/Footer.svelte";
 </script>
 
-<style>
-  .disabled {
-    background: gray;
-  }
-
-  .img {
-    width: 240px;
-  }
-</style>
-
-<main class="bg-red-700">
-  <button class="btn" class:disabled on:click={handleClick}> Click </button>
-  <p>{count}</p>
-</main>
-
-<img
-  class="img"
-  src="https://images.unsplash.com/photo-1593642634315-48f5414c3ad9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-  alt="" />
+<Navbar />
+<Landing />
+<Footer />
